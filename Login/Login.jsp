2f5c8174aa55f940d36d00a118f3638162b5e627
YoungJin.Heo <YoungJin.Heo98@gmail.com>
2022-05-21 20:40:31 +0900

    Day 3-2

--- Login/Login.jsp
+++ Login/Login.jsp
@@ -1,13 +1,36 @@
 <%@ page import="java.sql.*" %>
+<%@ page contentType="text/html; charset=utf-8" %>
 <%@ include file="../Conn/dbconn.jsp" %>
 
 <%
+    request.setCharacterEncoding("utf-8");
+
     String ID = request.getParameter("ID");
     String PWD = request.getParameter("PWD");
-    
+
+    ResultSet rs = null;
     Statement stmt = null;
+    String result = null;
 
     try{
-        String sql = ""
+        String sql = "SELECT 이름 FROM 회원 WHERE 회원번호 = "+ID+" AND 비밀번호 = '"+PWD+"'";
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery(sql);
+        if(rs.next()){
+            result = rs.getString("이름");
+        }
+        if(result != null){
+            out.println(result);
+        }
+    }catch(SQLException ex){
+        out.println("SQLException: "+ex.getMessage());
+    }finally{
+        if(rs!=null){
+            rs.close();
+        }
+        if(stmt != null)
+            stmt.close();
+        if(conn != null)
+            conn.close();
     }
     %>
--- template_js/Login_Check.js
+++ template_js/Login_Check.js
@@ -1,27 +1,44 @@
 $(document).ready(function(){
     function Login(e){ // 로그인 상태로 변경하는 함수
-        $("#Not_Login").css("visibility","hidden"); // 로그인 버튼 비활성화
+        $("#Not_Login").css("display","none"); // 로그인 버튼 비활성화
+        let div = document.createElement("div")
+        div.setAttribute("id","Success_Login")
+        let p = document.createElement("p");//p태그 생성
+        p.setAttribute("class","Font");
+        let text = document.createTextNode(e + "님 환영합니다.");
+        p.appendChild(text);
+        div.appendChild(p);
+        $("#Login_div").append(div);
     }
 
-    if(localStorage.getItem("user_num") != null){ // localStorage에 저장된 값이 있을 경우
-        const { Mnum } = JSON.parse(localStorage.getItem("user_num")); //회원 번호를 Mnum에 저장
-        Login(Mnum);
+    if(localStorage.getItem("user_info") != null){ // localStorage에 저장된 값이 있을 경우
+        const { name } = JSON.parse(localStorage.getItem("name")); //이름을 저장
+        Login(name);
     }else{// 로그인하지 않은 상태일 경우
         $("#Login_div").load("../template/Not_Login.html");
-        $("#Login_btn").on("click", function(){
-            var ID = $("#ID").val(); // 회원정보 가져옴
-            var PWD = $("#PWD").val(); // 비밀번호 가져옴
-
-            $.post( //  Login.jsp 파일에 정보 전달
-                "../Login/Login.jsp",
-                {
-                    ID: ID,
-                    PWD: PWD,
-                },
-                function(Result){//callback
-                    
-                }
-            )
-        })
     }
+
+    $(document).on("click","#Login_btn",function(){
+        var ID = $("#ID").val(); // 회원정보 가져옴
+        var PWD = $("#PWD").val(); // 비밀번호 가져옴
+
+        $.post( //  Login.jsp 파일에 정보 전달
+            "../Login/Login.jsp",
+            {
+                ID: ID,
+                PWD: PWD,
+            },
+            function(Result){//callback
+                if(Result != null){ //검색 결과가 있을 경우, 결과는 이름
+                    Login(Result);//로그인 함수 실행
+                    const user_info = {//로컬 스토리지에 저장하기 위함
+                        name: Result,
+                        Mnum: ID,
+                    }
+
+                    localStorage.setItem("user_info", JSON.stringify(user_info));//로컬 스토리지에 저장
+                }
+            }
+        )
+})
 })
