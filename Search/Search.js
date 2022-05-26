a544532e3d86fd838ef3b762be89ae2cae6a0e9c
YoungJin.Heo <YoungJin.Heo98@gmail.com>
2022-05-26 13:37:40 +0900

    Day 7

--- Search/Search.js
+++ Search/Search.js
@@ -1,37 +1,58 @@
 $(document).ready(function(){
+    //-------------------------functions------------------------------
+    function ShowResult(i){ //검색 결과를 보여주는 함수
+        $.post(
+            "../Search/Showing.jsp",//jsp파일에 정보 전달
+            {
+                Order: i,
+            },
+            function(Result){//callback
+                RemoveAllChild(); //결과를 보여줄 곳에 있는 값들 제거
+                CreateTable(Result); //결과에 따라 테이블 생성
+            }
+        )
+    }
+
     function CreateTable(array){ //테이블을 만드는 함수
         let test = array
         test=test.replace("[","");
         test=test.replace("]","");
         const arr = test.split(","); //,단위로 문장 split
-
-        console.log(arr);
-    }
-    
-
-    $("#Select_Showing").on("click",function(){ //상영중인 영화 클릭
-        $.post(
-            "../Search/Showing.jsp",//jsp파일에 정보 전달
-            {
-                Order: 1,
-            },
-            function(Result){//callback
-                CreateTable(Result);
+        console.log(test);
+        let Row = arr.length / 8;
+        let r = 0;
+        while(r < Row){
+            let Create_Tr = document.createElement("tr");
+            let div = document.getElementById("Result_Part");
+            div.appendChild(Create_Tr);
+            for(let i = 0; i<8; i++){
+                let Create_Td = document.createElement("td");
+                let Text = document.createTextNode(arr[r*8+i]);
+                Create_Td.appendChild(Text);
+                Create_Tr.appendChild(Create_Td);
             }
-        )
+            r++
+        }
+    }
+
+    function RemoveAllChild(){ //자식노드를 전부 지우는 함수. 검색 결과값 refresh 위함
+        let area = document.getElementById("Result_Part");
+        while(area.hasChildNodes()){
+            area.removeChild(area.firstChild);
+        }
+    }
+
+    //-------------------------PageLoad----------------------------
+    ShowResult(1); //페이지 로딩이 끝나면 상영중인 영화를 개봉일 오름차순으로 기본 표시
+
+    //-------------------------Events------------------------------
+    $("#Select_Showing").on("click",function(){ //상영중인 영화 클릭
+        ShowResult(1); //정렬방식 1로 검색 결과값 생성. 정렬방식 1은 개봉일 오름차순
     })
 
-    $("input[name=Order]").on("click",function(){
-        let val = $(this).val();
-        $.post(
-            "../Search/Showing.jsp",
-            {
-                Order: val,
-            },
-            function(Result){
-                CreateTable(Result);
-            }
-        )
+    $("input[name=Order]").on("click",function(){ //정렬 방식 클릭
+        let val = $(this).val(); //정렬 방식에 해당하는 값 가져옴
+        ShowResult(val);
     })
 
     $("#Select_Upcoming").on("click",function(){ //개봉 예정 영화 클릭
--- Search/Showing.jsp
+++ Search/Showing.jsp
@@ -6,29 +6,32 @@
 <%
     request.setCharacterEncoding("utf-8");
 
-    int Order = request.getParameter("Order");
+    int Order = Integer.parseInt(request.getParameter("Order"));
     ResultSet rs = null;
     Statement stmt = null;
     String[] str;
     int i = 0;
     int r = 0;
     int size = 0;
+    String sql = null;
 
     try{
         if(Order == 1){ //1이면 개봉일 오름차순 2이면 예매순위 오름차순
-            String sql = "SELECT * FROM 영화 WHERE 개봉일 >= (SYSDATE -7) AND SYSDATE >= 개봉일 ORDER BY 개봉일 ASC"; //상영중인 영화를 검색하는 SQL문
+            sql = "SELECT 영화.영화이름, 영화.장르, 영화.개봉일, 영화.감독, 영화.출연자, 영화.총상영시간, 영화.관람등급정보, 개별상세정보_상영중.예매자수 FROM 영화, 개별상세정보_상영중 WHERE 개봉일 >= (SYSDATE -7)  AND SYSDATE >= 개봉일  AND 영화.영화이름 = 개별상세정보_상영중.영화이름 ORDER BY 개봉일 ASC"; //상영중인 영화를 검색하는 SQL문
+        }else{
+            sql = "SELECT 영화.영화이름, 영화.장르, 영화.개봉일, 영화.감독, 영화.출연자, 영화.총상영시간, 영화.관람등급정보, 개별상세정보_상영중.예매자수 FROM 영화, 개별상세정보_상영중 WHERE 개봉일 >= (SYSDATE -7)  AND SYSDATE >= 개봉일  AND 영화.영화이름 = 개별상세정보_상영중.영화이름 ORDER BY 예매자수 DESC";
         }
         stmt = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
         rs = stmt.executeQuery(sql);
 
         rs.last(); //불러온 ReslutSet의 크기를 알기위함
         size = rs.getRow(); //불러온 ReslutSet의 행의 개수만큼 저장       
-        str = new String[size*7]; //ResultSet의 크기만큼 배열 선언 행의개수 * 열의 개수(영화의 열의 개수는 7개)
+        str = new String[size*8]; //ResultSet의 크기만큼 배열 선언 행의개수 * 열의 개수(영화의 열의 개수는 7개)
 
         rs.beforeFirst();
         while(rs.next()){
-            for(i=0; i<=6; i++){
-                str[r*7+i] = rs.getString(i+1); //ResultSet의 인덱스를 배열에 할당 ResultSet은 인덱스가 0이 아닌 1부터 시작하기에 +1
+            for(i=0; i<=7; i++){
+                str[r*8+i] = rs.getString(i+1); //ResultSet의 인덱스를 배열에 할당 ResultSet은 인덱스가 0이 아닌 1부터 시작하기에 +1
             }
             r++;
         }
